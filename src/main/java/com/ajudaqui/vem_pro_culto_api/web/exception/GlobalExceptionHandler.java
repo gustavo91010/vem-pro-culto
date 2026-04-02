13cfdbb178c1d4a60ba6c25bd8f97dc949580856
Gustavo <gustavo910@gmail.com>
2026-04-02 08:57:26 -0300

    GIF-17155 melhoria no rastreio de exception

--- src/main/java/com/ajudaqui/vem_pro_culto_api/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ajudaqui/vem_pro_culto_api/web/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.ajudaqui.vem_pro_culto_api.web.exception;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.coyote.BadRequestException;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -40,13 +43,33 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
   }
 
-  private void infoTrace(Exception exception) {
-    StackTraceElement element = exception.getStackTrace()[0];
-    StackTraceElement callElement = exception.getStackTrace()[1];
+  // private void infoTrace(Exception exception) {
+  // StackTraceElement element = exception.getStackTrace()[0];
+  // StackTraceElement callElement = exception.getStackTrace()[1];
 
-    log.error("Exception occurred at: [{}] {}, line: {} with error Details: [{}] {}, line: {} | {}",
-        callElement.getFileName(), callElement.getMethodName(),
-        callElement.getLineNumber(), element.getFileName(), element.getMethodName(),
-        element.getLineNumber(), exception.getMessage());
+  // log.error("Exception occurred at: [{}] {}, line: {} with error Details: [{}]
+  // {}, line: {} | {}",
+  // callElement.getFileName(), callElement.getMethodName(),
+  // callElement.getLineNumber(), element.getFileName(), element.getMethodName(),
+  // element.getLineNumber(), exception.getMessage());
+  // }
+
+  private void infoTrace(Exception exception) {
+    StackTraceElement[] fullTrace = exception.getStackTrace();
+
+    List<StackTraceElement> projectTrace = Arrays.stream(fullTrace)
+        .filter(e -> e.getClassName().startsWith("br.com.gif"))
+        .toList();
+
+    StringBuilder sb = new StringBuilder("Project stack trace:\n");
+    for (int i = 0; i < projectTrace.size(); i++) {
+      StackTraceElement e = projectTrace.get(i);
+      sb.append(String.format("  [%d] %s.%s (line %d)%n",
+          i, e.getFileName(), e.getMethodName(), e.getLineNumber()));
+    }
+    sb.append("Message: ").append(exception.getMessage());
+
+    log.error(sb.toString());
   }
+
 }
