3d48ad69d11cf4a7192705b1162fd908075389e5
Gustavo <gustavo910@gmail.com>
2026-04-05 01:58:05 -0300

    fluxo de remocao de vinculo

--- src/main/java/com/ajudaqui/vem_pro_culto_api/web/controller/IgrejaController.java
+++ src/main/java/com/ajudaqui/vem_pro_culto_api/web/controller/IgrejaController.java
@@ -56,7 +56,7 @@ public class IgrejaController {
       @PathVariable String razaoSocial,
       @RequestHeader(value = "Authorization", required = false) String jwtToken) {
 
-    if (!jwtToken.isBlank())
+    if (jwtToken != null && !jwtToken.isBlank())
       jwtToken = jwtUtils.getAccessToken(jwtToken);
     Igreja igreja = igrejaService.buscarPorRazaoSocial(razaoSocial, jwtToken);
     return ResponseEntity.ok(new IgrejaResponse(igreja));
